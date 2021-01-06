da7b12d161ea6959f8eb43c75c1764feae00ea30
jameschu <jameschu@umich.edu>
2021-01-06 12:27:00 -0500

    Objects

--- Ex_Files_JavaScript_EssT/Exercise Files/04_12/script.js
+++ Ex_Files_JavaScript_EssT/Exercise Files/04_12/script.js
@@ -1,11 +1,48 @@
-function doSomeMath() {
-	var a = 5;
-	var b = 4;
-	var sum = a + b;
+// function doSomeMath() {
+// 	var a = 5;
+// 	var b = 4;
+// 	// var sum = a + b;
 
-	return sum;
+// 	// return sum;
+// 	function multiply() { 
+// 		var result = a*b;
+// 		return result;
+// 	}
+// 	return multiply;
+// }
+
+// var theResult = doSomeMath();
+
+// console.log("The result: ", theResult);
+
+
+
+function giveMeEms(pixels) { 
+	var base = 16; 
+
+	function doMath() { 
+		return pixels/base;
+	}
+	return doMath;
 }
 
-var theResult = doSomeMath();
+var smallSize = giveMeEms(12);
+var mediumSize = giveMeEms(18);
+var largeSize = giveMeEms(24);
+var XLSize = giveMeEms(32);
 
-console.log("The result: ", theResult);
+console.log("Small: ", smallSize);
+console.log("Med: ", mediumSize);
+console.log("L: ", largeSize);
+console.log("XL: ", XLSize);
+
+
+
+
+
+
+
+
+
+
+
