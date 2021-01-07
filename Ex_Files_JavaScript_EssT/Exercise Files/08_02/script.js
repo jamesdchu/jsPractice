ae22261a3a56e3248a12e087f04598bbbe44f9ab
jameschu <jameschu@umich.edu>
2021-01-07 12:51:27 -0500

    Typing speed

--- Ex_Files_JavaScript_EssT/Exercise Files/08_02/script.js
+++ Ex_Files_JavaScript_EssT/Exercise Files/08_02/script.js
@@ -12,12 +12,23 @@ const theTimer = document.querySelector(".timer");
 
 
 // Match the text entered with the provided text on the page:
-
+function spellCheck() { 
+    let textEntered = testArea.value;
+    console.log(textEntered);
+}
 
 // Start the timer:
-
+function start() { 
+    let textEnteredLength = testArea.nodeValue.length;
+    console.log(textEnteredLength);
+}
 
 // Reset everything:
-
+function reset() { 
+    console.log("resetted")
+}
 
 // Event listeners for keyboard input and the reset button:
+testArea.addEventListener("keypress", start, false);
+testArea.addEventListener("keyup", spellCheck, false);
+resetButton.addEventListener("click",reset,false);
