81212bba9af543baa1d192b8ec128d980678d6b9
jameschu <jameschu@umich.edu>
2021-01-10 20:16:42 -0500

    Creating React components

--- react-essentials/src/App.js
+++ react-essentials/src/App.js
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Header() { 
+  return (
+    <header>
+      <h1>James' Kitchen</h1> 
+    </header>
+  )
+}
+
+function Main() { 
+  return ( 
+    <section> 
+      <p>We serve the best top tier food around!</p>
+    </section>
+  )
+}
+
+function Footer() { 
+  return ( 
+    <footer> 
+      <p>Zero cap</p>
+    </footer>
+  )
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
