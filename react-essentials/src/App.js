30b834a4cb0bfcccbe46aaa052ebad85c3a29419
jameschu <jameschu@umich.edu>
2021-01-12 22:58:17 -0500

    Learning and working with useEffect

--- react-essentials/src/App.js
+++ react-essentials/src/App.js
@@ -1,14 +1,24 @@
 // import logo from './logo.svg';
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './App.css';
 
 function App({authorized}) {
   const [emotion, setEmotion] = useState("happy");
-  console.log(emotion);
+  const [secondary, setSecondary] = useState("tired");
+  
+  useEffect(() => { 
+    console.log('Its ${emotion} around here!');
+  }, [emotion]);
+
+  useEffect(() => { 
+    console.log("It's ${secondary} around here!");
+  }, [secondary]);
+
   return (
   <> 
-    <h1>Current emotion is {emotion}.</h1>;
+    <h1>Current emotion is {emotion} and {secondary}.</h1>;
     <button onClick={() => setEmotion("happy")}> Happy </button>
+    <button onCLick={() => setSecondary("crabby")}>Make Crabby</button>
     <button onClick={() => setEmotion("frustrated")}>Frustrate</button>
     <button onClick={() => setEmotion("enthusiastic")}>Enthuse</button>
   </> 
