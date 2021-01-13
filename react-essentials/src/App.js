285c2d8d8da8d88c97591b27d4adf61eff6f35c2
jameschu <jameschu@umich.edu>
2021-01-12 22:51:46 -0500

    Starting to understand useStae to keep track of local memory

--- react-essentials/src/App.js
+++ react-essentials/src/App.js
@@ -3,11 +3,14 @@ import React, { useState } from "react";
 import './App.css';
 
 function App({authorized}) {
-  const what = useState();
-  console.log(what);
+  const [emotion, setEmotion] = useState("happy");
+  console.log(emotion);
   return (
   <> 
-    <h1>Hello</h1>
+    <h1>Current emotion is {emotion}.</h1>;
+    <button onClick={() => setEmotion("happy")}> Happy </button>
+    <button onClick={() => setEmotion("frustrated")}>Frustrate</button>
+    <button onClick={() => setEmotion("enthusiastic")}>Enthuse</button>
   </> 
   );
 }
